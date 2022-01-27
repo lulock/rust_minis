fa540aa67fa10b7201f27b978001430117420021
lulock <leilamethnani@gmail.com>
2022-01-27 10:16:24 +0400

    implemented states Menu, InGame, Pause

--- breakout_pong/src/main_old.rs
+++ breakout_pong/src/main_old.rs
@@ -4,54 +4,54 @@ use bevy::{
     sprite::collide_aabb::{collide, Collision},
 };
 
-/// This example illustrates how to use [`States`] to control transitioning from a `Menu` state to
-/// an `InGame` state.
-/// 
-/// 
+/// Stage for systems
+pub const GAME_STATE_STAGE: &str = "game_state_stage";
+
+/// Game States
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum GameState {
+    Menu,
+    Playing,
+    Pause,
+}
+
+/// A mash-up implementation of the classic games "Breakout" and "Pong"
+const TIME_STEP: f32 = 1.0 / 60.0;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_state(AppState::Menu)
+        .add_state(GameState::Playing)
+        .insert_resource(Scoreboard {
+            score1: 0,
+            score2: 0,
+        })
+
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
-        .add_system_set(SystemSet::on_enter(AppState::Menu).with_system(setup_menu))
-        .add_system_set(SystemSet::on_update(AppState::Menu).with_system(menu))
-        .add_system_set(SystemSet::on_exit(AppState::Menu).with_system(cleanup_menu))
+        .add_system_set(SystemSet::on_enter(GameState::Menu).with_system(setup_menu))
+        .add_system_set(SystemSet::on_update(GameState::Menu).with_system(menu))
+        .add_system_set(SystemSet::on_exit(GameState::Menu).with_system(cleanup_menu))
+        .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(setup))
 
-        .add_system_set(SystemSet::on_enter(AppState::Pause).with_system(setup_menu))
-        .add_system_set(SystemSet::on_update(AppState::Pause).with_system(pause))
-        .add_system_set(SystemSet::on_exit(AppState::Pause).with_system(cleanup_menu))
-
-        .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(setup))
+        // .add_startup_system(setup)
+        // systems to run only while Playing
         .add_system_set(
-            SystemSet::on_update(AppState::InGame)
-                // .with_system(movement)
-                // .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
+            SystemSet::on_update(GameState::Playing)
+                .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
                 .with_system(paddle1_movement_system)
                 .with_system(paddle2_movement_system)
-                // .with_system(scoreboard_system)
-                .with_system(ball_movement_system)
                 .with_system(ball_collision_system)
-                .with_system(change_color),
+                .with_system(ball_movement_system)
+                .with_system(scoreboard_system)
+                .with_system(space_to_pause),
         )
         .add_system(bevy::input::system::exit_on_esc_system)
-        .add_system(space_to_pause)
-
         .run();
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-enum AppState {
-    Menu,
-    InGame,
-    Pause,
 }
 
 struct MenuData {
     button_entity: Entity,
 }
 
-/// A mash-up implementation of the classic games "Breakout" and "Pong"
-const TIME_STEP: f32 = 1.0 / 60.0;
 const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
@@ -92,8 +92,9 @@ fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(MenuData { button_entity });
 }
 
+
 fn menu(
-    mut state: ResMut<State<AppState>>,
+    mut state: ResMut<State<GameState>>,
     mut interaction_query: Query<
         (&Interaction, &mut UiColor),
         (Changed<Interaction>, With<Button>),
@@ -103,30 +104,7 @@ fn menu(
         match *interaction {
             Interaction::Clicked => {
                 *color = PRESSED_BUTTON.into();
-                state.set(AppState::InGame).unwrap();
-            }
-            Interaction::Hovered => {
-                *color = HOVERED_BUTTON.into();
-            }
-            Interaction::None => {
-                *color = NORMAL_BUTTON.into();
-            }
-        }
-    }
-}
-fn pause(
-    mut state: ResMut<State<AppState>>,
-    mut interaction_query: Query<
-        (&Interaction, &mut UiColor),
-        (Changed<Interaction>, With<Button>),
-    >,
-) {
-    for (interaction, mut color) in interaction_query.iter_mut() {
-        match *interaction {
-            Interaction::Clicked => {
-                *color = PRESSED_BUTTON.into();
-                state.pop().unwrap();
-                // state.set(AppState::InGame).unwrap();
+                state.set(GameState::Playing).unwrap();
             }
             Interaction::Hovered => {
                 *color = HOVERED_BUTTON.into();
@@ -141,14 +119,6 @@ fn pause(
 fn cleanup_menu(mut commands: Commands, menu_data: Res<MenuData>) {
     commands.entity(menu_data.button_entity).despawn_recursive();
 }
-
-// fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
-//     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-//     commands.spawn_bundle(SpriteBundle {
-//         texture: asset_server.load("branding/icon.png"),
-//         ..Default::default()
-//     });
-// }
 
 
 #[derive(Component)]
@@ -401,6 +371,39 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             })
             .insert(Collider::Scorable2);
     }
+    // let brick_rows = 1;
+    // let brick_columns = 1;
+    // let brick_spacing = 20.0;
+    // let brick_size = Vec3::new(10.0, 150.0, 1.0);
+    // let bricks_width = brick_columns as f32 * (brick_size.x + brick_spacing) - brick_spacing;
+    // // center the bricks and move them up a bit
+    // let bricks_offset = Vec3::new(-(bricks_width - brick_size.x) / 2.0, 100.0, 0.0);
+    // let brick_color = Color::rgb(0.5, 0.5, 1.0);
+    // for row in 0..brick_rows {
+    //     let y_position = row as f32 * (brick_size.y + brick_spacing);
+    //     for column in 0..brick_columns {
+    //         let brick_position = Vec3::new(
+    //             column as f32 * (brick_size.x + brick_spacing),
+    //             y_position,
+    //             0.0,
+    //         ) + bricks_offset;
+    //         // brick
+    //         commands
+    //             .spawn_bundle(SpriteBundle {
+    //                 sprite: Sprite {
+    //                     color: brick_color,
+    //                     ..Default::default()
+    //                 },
+    //                 transform: Transform {
+    //                     translation: brick_position,
+    //                     scale: brick_size,
+    //                     ..Default::default()
+    //                 },
+    //                 ..Default::default()
+    //             })
+    //             .insert(Collider::Scorable);
+    //     }
+    // }
 }
 
 fn paddle1_movement_system(
@@ -445,55 +448,51 @@ fn paddle2_movement_system(
     translation.y = translation.y.min(220.0).max(-220.0);
 }
 
-// fn scoreboard_system(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
-//     let mut text = query.single_mut();
-//     text.sections[1].value = format!("{}", scoreboard.score1);
-//     text.sections[3].value = format!("{}", scoreboard.score2);
-// }
+fn ball_movement_system(mut ball_query: Query<(&Ball, &mut Transform)>, game_state: Res<State<GameState>>,) {
+    match game_state.current() {
+        GameState::Playing => {
+            let (ball, mut transform) = ball_query.single_mut();
+            transform.translation += ball.velocity * TIME_STEP;
+        }
+        GameState::Pause => {
 
-fn ball_movement_system(mut ball_query: Query<(&Ball, &mut Transform)>) {
-    let (ball, mut transform) = ball_query.single_mut();
-    transform.translation += ball.velocity * TIME_STEP;
-}
+        }
+        GameState::Menu => {
 
-const SPEED: f32 = 100.0;
-fn movement(
-    time: Res<Time>,
-    input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Sprite>>,
-) {
-    for mut transform in query.iter_mut() {
-        let mut direction = Vec3::ZERO;
-        if input.pressed(KeyCode::Left) {
-            direction.x -= 1.0;
-        }
-        if input.pressed(KeyCode::Right) {
-            direction.x += 1.0;
-        }
-        if input.pressed(KeyCode::Up) {
-            direction.y += 1.0;
-        }
-        if input.pressed(KeyCode::Down) {
-            direction.y -= 1.0;
-        }
-
-        if direction != Vec3::ZERO {
-            transform.translation += direction.normalize() * SPEED * time.delta_seconds();
         }
     }
 }
 
-fn change_color(time: Res<Time>, mut query: Query<&mut Sprite>) {
-    for mut sprite in query.iter_mut() {
-        sprite
-            .color
-            .set_b((time.seconds_since_startup() * 0.5).sin() as f32 + 2.0);
+fn scoreboard_system(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
+    let mut text = query.single_mut();
+    text.sections[1].value = format!("{}", scoreboard.score1);
+    text.sections[3].value = format!("{}", scoreboard.score2);
+}
+
+fn space_to_pause(
+    mut keys: ResMut<Input<KeyCode>>,
+    mut game_state: ResMut<State<GameState>>,
+) {
+    if keys.just_pressed(KeyCode::Space) {
+        match game_state.current() {
+            GameState::Playing => {
+                game_state.push(GameState::Menu).unwrap();
+            }
+            GameState::Menu => {
+                game_state.pop().unwrap();
+            }
+            GameState::Pause => {
+            
+            }
+        }
+        println!("{:?}", *game_state.current());
+        keys.reset(KeyCode::Space);
     }
 }
 
 fn ball_collision_system(
     mut commands: Commands,
-    // mut scoreboard: ResMut<Scoreboard>,
+    mut scoreboard: ResMut<Scoreboard>,
     mut ball_query: Query<(&mut Ball, &Transform)>,
     collider_query: Query<(Entity, &Collider, &Transform)>,
 ) {
@@ -512,11 +511,11 @@ fn ball_collision_system(
         if let Some(collision) = collision {
             // scorable colliders should be despawned and increment the scoreboard on collision
             if let Collider::Scorable1 = *collider {
-                // scoreboard.score1 += 1;
+                scoreboard.score1 += 1;
                 commands.entity(collider_entity).despawn();
             }
             if let Collider::Scorable2 = *collider {
-                // scoreboard.score2 += 1;
+                scoreboard.score2 += 1;
                 commands.entity(collider_entity).despawn();
             }
 
@@ -549,27 +548,5 @@ fn ball_collision_system(
                 break;
             }
         }
-    }
-}
-
-fn space_to_pause(
-    mut keys: ResMut<Input<KeyCode>>,
-    mut game_state: ResMut<State<AppState>>,
-) {
-    if keys.just_pressed(KeyCode::Space) {
-        match game_state.current() {
-            AppState::InGame => {
-                game_state.push(AppState::Pause).unwrap();
-            }
-            AppState::Pause => {
-                game_state.pop().unwrap();
-            }
-            AppState::Menu => {
-
-            }
-
-        }
-        println!("{:?}", *game_state.current());
-        keys.reset(KeyCode::Space);
     }
 }
